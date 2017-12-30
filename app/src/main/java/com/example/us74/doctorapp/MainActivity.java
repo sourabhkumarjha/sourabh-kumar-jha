29cb7cac4db5b87d9ddefb00c2da09e83d033c2c
US74 <sourabhkumarjha2196@gmail.com>
2017-12-30 13:31:50 +0530

    commit

---
+++ app/src/main/java/com/example/us74/doctorapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.us74.doctorapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
